ef1157550f4d7f4bdcfd2532a11e37ee1f98424c
pankaj1707k <pankaj.1707k@gmail.com>
2022-04-18 23:03:58 +0530

    HomeView with title in context

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
 from . import views
-from django.views.generic import TemplateView
 
-urlpatterns = [path("", TemplateView.as_view(template_name="main/home.html"))]
+urlpatterns = [path("", views.HomeView.as_view())]
