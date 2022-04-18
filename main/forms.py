063a31cb5325d07dc1fa6fbd9c99fed9e59a134f
pankaj1707k <pankaj.1707k@gmail.com>
2022-04-19 01:17:37 +0530

    User registration module

---
+++ main/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Username"}
+        ),
+    )
+    email = forms.EmailField(
+        label="Email",
+        widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "name@example.com"}
+        ),
+    )
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Password"}
+        ),
+    )
+    password2 = forms.CharField(
+        label="Confirm password",
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Retype password"}
+        ),
+    )
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
+from django.views import View
+from .forms import UserRegisterForm
 
 
 class HomeView(TemplateView):
@@ -9,3 +11,28 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["title"] = "tOdO | Home"
         return context
+
+
+class RegisterView(TemplateView):
+    template_name = "main/register.html"
+    form_class = UserRegisterForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "tOdO | Register"
+        return context
+
+    def get(self, request):
+        form = self.form_class()
+        context = self.get_context_data()
+        context.update({"form": form})
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+        context = self.get_context_data()
+        context.update({"form": form})
+        return render(request, self.template_name, context)
