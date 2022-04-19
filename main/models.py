5830d8c4fa38c199f4dce329665a72a8a0e9567a
pankaj1707k <pankaj.1707k@gmail.com>
2022-04-19 17:15:44 +0530

    Created model Task and registered on admin site

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Task(models.Model):
+    task = models.TextField(_("task"))
+    due_date = models.DateField(_("due date"), blank=True, null=True)
+    date_added = models.DateTimeField(_("date added"), auto_now_add=True)
+    completed = models.BooleanField(_("completed"), default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (
+            f"#{self.id}: {self.task if len(self.task)<20 else (self.task[:20]+'...')}"
+        )
