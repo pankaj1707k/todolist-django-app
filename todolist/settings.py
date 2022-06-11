678b651d7c733bd2c3c282f7a20f69f05a003a4f
pankaj1707k <pankaj.1707k@gmail.com>
2022-06-11 12:40:17 +0530

    Added deploy domain to ALLOWED_HOSTS

--- todolist/settings.py
+++ todolist/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1:8000"]
+ALLOWED_HOSTS = ["127.0.0.1:8000", "tasklistdjango.herokuapp.com"]
 
 
 # Application definition
