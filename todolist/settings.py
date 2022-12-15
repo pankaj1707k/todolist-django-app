b14e49786d68c4aa2e9ea3103fa814a5df2e073b
pankaj1707k <pankaj.1707k@gmail.com>
2022-12-15 12:13:28 +0530

    config: add replit deployment configuration

--- todolist/settings.py
+++ todolist/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+
 import environ
-import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1:8000", "tasklistdjango.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1:8000", "localhost:8000"]
 
 
 # Application definition
@@ -129,4 +129,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-django_on_heroku.settings(locals())
+X_FRAME_OPTIONS = "*"
